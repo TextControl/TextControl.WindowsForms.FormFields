b13d49cc9bd388eae19a4f9d7d475d1d540b11d9
Gunnar Giffey <gunnar@textcontrol.com>
2018-06-29 11:00:09 +0200

    Added initial project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace tx_formfield_editor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textControl1 = new TXTextControl.TextControl();
+            this.buttonBar1 = new TXTextControl.ButtonBar();
+            this.statusBar1 = new TXTextControl.StatusBar();
+            this.rulerBar1 = new TXTextControl.RulerBar();
+            this.rulerBar2 = new TXTextControl.RulerBar();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.formFieldsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.formTextToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.formCheckBoxToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.formDropDownToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textControl1
+            // 
+            this.textControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textControl1.Font = new System.Drawing.Font("Arial", 10F);
+            this.textControl1.Location = new System.Drawing.Point(25, 77);
+            this.textControl1.Name = "textControl1";
+            this.textControl1.PageMargins.Bottom = 79.03D;
+            this.textControl1.PageMargins.Left = 79.03D;
+            this.textControl1.PageMargins.Right = 79.03D;
+            this.textControl1.PageMargins.Top = 79.03D;
+            this.textControl1.Size = new System.Drawing.Size(983, 662);
+            this.textControl1.TabIndex = 0;
+            this.textControl1.UserNames = null;
+            this.textControl1.TextFieldClicked += new TXTextControl.TextFieldEventHandler(this.textControl1_TextFieldClicked);
+            // 
+            // buttonBar1
+            // 
+            this.buttonBar1.BackColor = System.Drawing.SystemColors.Control;
+            this.buttonBar1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.buttonBar1.Location = new System.Drawing.Point(0, 24);
+            this.buttonBar1.Name = "buttonBar1";
+            this.buttonBar1.Size = new System.Drawing.Size(1008, 28);
+            this.buttonBar1.TabIndex = 1;
+            this.buttonBar1.Text = "buttonBar1";
+            // 
+            // statusBar1
+            // 
+            this.statusBar1.BackColor = System.Drawing.SystemColors.Control;
+            this.statusBar1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.statusBar1.Location = new System.Drawing.Point(0, 739);
+            this.statusBar1.Name = "statusBar1";
+            this.statusBar1.Size = new System.Drawing.Size(1008, 22);
+            this.statusBar1.TabIndex = 2;
+            // 
+            // rulerBar1
+            // 
+            this.rulerBar1.Alignment = TXTextControl.RulerBarAlignment.Left;
+            this.rulerBar1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.rulerBar1.Location = new System.Drawing.Point(0, 77);
+            this.rulerBar1.Name = "rulerBar1";
+            this.rulerBar1.Size = new System.Drawing.Size(25, 662);
+            this.rulerBar1.TabIndex = 3;
+            this.rulerBar1.Text = "rulerBar1";
+            // 
+            // rulerBar2
+            // 
+            this.rulerBar2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.rulerBar2.Location = new System.Drawing.Point(0, 52);
+            this.rulerBar2.Name = "rulerBar2";
+            this.rulerBar2.Size = new System.Drawing.Size(1008, 25);
+            this.rulerBar2.TabIndex = 4;
+            this.rulerBar2.Text = "rulerBar2";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.formFieldsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1008, 24);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // formFieldsToolStripMenuItem
+            // 
+            this.formFieldsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.formTextToolStripMenuItem,
+            this.formCheckBoxToolStripMenuItem,
+            this.formDropDownToolStripMenuItem,
+            this.dateToolStripMenuItem});
+            this.formFieldsToolStripMenuItem.Name = "formFieldsToolStripMenuItem";
+            this.formFieldsToolStripMenuItem.Size = new System.Drawing.Size(97, 20);
+            this.formFieldsToolStripMenuItem.Text = "Add FormField";
+            // 
+            // formTextToolStripMenuItem
+            // 
+            this.formTextToolStripMenuItem.Name = "formTextToolStripMenuItem";
+            this.formTextToolStripMenuItem.Size = new System.Drawing.Size(159, 22);
+            this.formTextToolStripMenuItem.Text = "FormText";
+            this.formTextToolStripMenuItem.Click += new System.EventHandler(this.formTextToolStripMenuItem_Click);
+            // 
+            // formCheckBoxToolStripMenuItem
+            // 
+            this.formCheckBoxToolStripMenuItem.Name = "formCheckBoxToolStripMenuItem";
+            this.formCheckBoxToolStripMenuItem.Size = new System.Drawing.Size(159, 22);
+            this.formCheckBoxToolStripMenuItem.Text = "FormCheckBox";
+            this.formCheckBoxToolStripMenuItem.Click += new System.EventHandler(this.formCheckBoxToolStripMenuItem_Click);
+            // 
+            // formDropDownToolStripMenuItem
+            // 
+            this.formDropDownToolStripMenuItem.Name = "formDropDownToolStripMenuItem";
+            this.formDropDownToolStripMenuItem.Size = new System.Drawing.Size(159, 22);
+            this.formDropDownToolStripMenuItem.Text = "FormDropDown";
+            this.formDropDownToolStripMenuItem.Click += new System.EventHandler(this.formDropDownToolStripMenuItem_Click);
+            // 
+            // dateToolStripMenuItem
+            // 
+            this.dateToolStripMenuItem.Name = "dateToolStripMenuItem";
+            this.dateToolStripMenuItem.Size = new System.Drawing.Size(159, 22);
+            this.dateToolStripMenuItem.Text = "Date";
+            this.dateToolStripMenuItem.Click += new System.EventHandler(this.dateToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1008, 761);
+            this.Controls.Add(this.textControl1);
+            this.Controls.Add(this.rulerBar1);
+            this.Controls.Add(this.rulerBar2);
+            this.Controls.Add(this.buttonBar1);
+            this.Controls.Add(this.statusBar1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "TX Text Control FormField Sample";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TXTextControl.TextControl textControl1;
+        private TXTextControl.ButtonBar buttonBar1;
+        private TXTextControl.StatusBar statusBar1;
+        private TXTextControl.RulerBar rulerBar1;
+        private TXTextControl.RulerBar rulerBar2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem formFieldsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem formTextToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem formCheckBoxToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem formDropDownToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dateToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using TXTextControl.DocumentServer.Fields;
+
+namespace tx_formfield_editor
+{
+    public partial class Form1 : Form
+    {
+        private int dpiX = 15;
+
+        // unicode strings for the checkbox characters
+        private string UNCHECKED = UnicodeHexToString("\\u2610");
+        private string CHECKED = UnicodeHexToString("\\u2612");
+
+        private static string UnicodeHexToString(string text)
+        {
+            // returns the string representation
+            return System.Text.Encoding.Unicode.GetString(BitConverter.GetBytes(short.Parse(text.Substring(2), System.Globalization.NumberStyles.HexNumber)));
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            textControl1.RulerBar = rulerBar2;
+            textControl1.ButtonBar = buttonBar1;
+            textControl1.StatusBar = statusBar1;
+            textControl1.VerticalRulerBar = rulerBar1;
+
+            dpiX = (int)(1440 / textControl1.CreateGraphics().DpiX);
+        }
+
+        private void formTextToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // create a new FormText field and add it to TextControl
+            FormText tbField = new FormText();
+
+            tbField.ApplicationField.Deleteable = false;
+            tbField.ApplicationField.Editable = false;
+            tbField.ApplicationField.DoubledInputPosition = true;
+
+            textControl1.ApplicationFields.Add(tbField.ApplicationField);
+        }
+
+        private void formCheckBoxToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // create a new FormCheckBox field and add it to TextControl
+            FormCheckBox fcbField = new FormCheckBox();
+            fcbField.Checked = false;
+
+            fcbField.Text = fcbField.Checked == true ? CHECKED : UNCHECKED;
+
+            fcbField.ApplicationField.Deleteable = false;
+            fcbField.ApplicationField.Editable = false;
+            fcbField.ApplicationField.DoubledInputPosition = true;
+
+            textControl1.ApplicationFields.Add(fcbField.ApplicationField);
+        }
+
+        private void formDropDownToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // create a new FormDropDown field and add it to TextControl
+            FormDropDown fddField = new FormDropDown();
+            
+            fddField.ApplicationField.Deleteable = false;
+            fddField.ApplicationField.Editable = false;
+            fddField.ApplicationField.DoubledInputPosition = true;
+
+            // open the dialog to allow user to add some items
+            fddField.ShowDialog();
+            fddField.Text = fddField.ListEntries.ElementAt(0);
+
+            textControl1.ApplicationFields.Add(fddField.ApplicationField);
+        }
+
+        private void dateToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // create a new DateField field and add it to TextControl
+            DateField dField = new DateField();
+
+            dField.ApplicationField.Deleteable = false;
+            dField.ApplicationField.Editable = false;
+            dField.ApplicationField.DoubledInputPosition = true;
+
+            // set the current date text to today
+            dField.Date = DateTime.Today;
+
+            // show the dialog to choose the string format of the field
+            dField.ShowDialog();
+            textControl1.ApplicationFields.Add(dField.ApplicationField);
+        }
+
+        private void textControl1_TextFieldClicked(object sender, TXTextControl.TextFieldEventArgs e)
+        {
+            TXTextControl.ApplicationField appField = (TXTextControl.ApplicationField)e.TextField;
+            ApplicationFieldActivate(appField);
+        }
+
+        private void ApplicationFieldActivate(TXTextControl.ApplicationField ApplicationField)
+        {
+            // remove all child controls
+            textControl1.Controls.Clear();
+
+            // check the field type in order to display the proper control
+            switch (ApplicationField.TypeName)
+            {
+                case "FORMDROPDOWN":
+                    // create a FormDropDown field
+                    FormDropDown fddField = new FormDropDown(ApplicationField);
+
+                    // create a new System.Windows.Forms.ComboBox and add the
+                    // FormDropDown's items
+                    ComboBox cb = new ComboBox();
+
+                    cb.Items.AddRange(fddField.ListEntries.ToArray<string>());
+
+                    Point cbLocation = new Point((fddField.ApplicationField.Bounds.Location.X - 
+                                                 textControl1.ScrollLocation.X) / dpiX, 
+                                                 (fddField.ApplicationField.Bounds.Location.Y - 
+                                                 textControl1.ScrollLocation.Y) / dpiX);
+
+                    cb.Location = cbLocation;
+                    cb.Tag = fddField;
+                    cb.SelectedText = fddField.ApplicationField.Text;
+                    cb.SelectedIndexChanged += new EventHandler(cb_SelectedIndexChanged);
+                    cb.LostFocus += new EventHandler(cb_LostFocus);
+
+                    // show the ComboBox on top of TextControl
+                    textControl1.Controls.Add(cb);
+                    cb.Focus();
+
+                    break;
+
+                case "FORMCHECKBOX":
+                    // create a FormCheckBox field
+                    FormCheckBox fcbField = new FormCheckBox(ApplicationField);
+
+                    fcbField.Checked = !fcbField.Checked;
+                    // change the text according to the current state
+                    fcbField.Text = fcbField.Checked == true ? CHECKED : UNCHECKED;
+                    break;
+
+                case "DATE":
+                    // create a DateField field
+                    DateField dField = new DateField(ApplicationField);
+
+                    // create a new System.Windows.Forms.DateTimePicker and set the
+                    // date to the current DateField date
+                    DateTimePicker dtPicker = new DateTimePicker();
+                    dtPicker.Tag = dField;
+                    dtPicker.Value = DateTime.Parse(dField.Text);
+
+                    Point dtpLocation = new Point((dField.ApplicationField.Bounds.Location.X -
+                                                  textControl1.ScrollLocation.X) / dpiX,
+                                                  (dField.ApplicationField.Bounds.Location.Y -
+                                                  textControl1.ScrollLocation.Y) / dpiX);
+
+                    dtPicker.Location = dtpLocation;
+
+                    dtPicker.ValueChanged += new EventHandler(dtPicker_ValueChanged);
+                    dtPicker.LostFocus += new EventHandler(dtPicker_LostFocus);
+
+                    // show the DateTimePicker on top of TextControl
+                    textControl1.Controls.Add(dtPicker);
+                    dtPicker.Focus();
+
+                    break;
+
+                case "FORMTEXT":
+                    // create a FormText field
+                    FormText ftField = new FormText(ApplicationField);
+
+                    // create a new System.Windows.Forms.TextBox and set the
+                    // text to the current FormText text
+                    TextBox tb = new TextBox();
+
+                    tb.Text = ftField.Text;
+                    ftField.Text = "";
+
+                    Point tbLocation = new Point((ftField.ApplicationField.Bounds.Location.X -
+                                                  textControl1.ScrollLocation.X) / dpiX,
+                                                  (ftField.ApplicationField.Bounds.Location.Y -
+                                                  textControl1.ScrollLocation.Y) / dpiX);
+
+                    tb.Location = tbLocation;
+                    tb.Tag = ftField;
+                    tb.LostFocus += new EventHandler(tb_LostFocus);
+                    tb.KeyPress += new KeyPressEventHandler(tb_KeyPress);
+                    tb.SelectAll();
+
+                    // show the TextBox on top of TextControl
+                    textControl1.Controls.Add(tb);
+                    tb.Focus();
+                    break;
+            }
+        }
+
+        void dtPicker_LostFocus(object sender, EventArgs e)
+        {
+            // update the DateField and remove child controls
+            ((DateField)((DateTimePicker)sender).Tag).Date = ((DateTimePicker)sender).Value;
+            textControl1.Controls.Clear();
+        }
+
+        void dtPicker_ValueChanged(object sender, EventArgs e)
+        {
+            // update the DateField and remove child controls
+            ((DateField)((DateTimePicker)sender).Tag).Date = ((DateTimePicker)sender).Value;
+            textControl1.Controls.Clear();
+        }
+
+        void cb_LostFocus(object sender, EventArgs e)
+        {
+            // update the FormDropDown and remove child controls
+            ((FormDropDown)((ComboBox)sender).Tag).Text = ((ComboBox)sender).Text;
+            textControl1.Controls.Clear();
+        }
+
+        void cb_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // update the FormDropDown and remove child controls
+            ((FormDropDown)((ComboBox)sender).Tag).Text = ((ComboBox)sender).SelectedItem.ToString();
+            textControl1.Controls.Clear();
+        }
+
+        void tb_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // update the FormText on pressing ENTER and remove child controls
+            if (e.KeyChar != 13)
+                return;
+
+            ((FormText)((TextBox)sender).Tag).Text = ((TextBox)sender).Text;
+            textControl1.Controls.Clear();
+        }
+
+        void tb_LostFocus(object sender, EventArgs e)
+        {
+            // update the FormText and remove child controls
+            ((FormText)((TextBox)sender).Tag).Text = ((TextBox)sender).Text;
+            textControl1.Controls.Clear();
+        }
+    }
+}
